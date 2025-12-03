187be21d5869618f5768ebe61f3ae9c2ae7fb619
iemand <247100938+IFCW65@users.noreply.github.com>
2025-12-03 17:00:52 +0100

    Update cypher.java

--- cypher.java
+++ cypher.java
@@ -1,51 +1,25 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner toetsenbord = new Scanner(System.in);
-        int hoofdletter = 1, kleineLetters= 0, cijfers = 0, spaties = 1, teller = 0;
-        char begin, einde;
-        boolean beginLetter, leesteken;
-        String zin, zinCopy = "";
+        String woord;
+        int teller = 0;
+        char karakter;
+        int key;
+        String cypher = "";
 
-        do {
-            System.out.print("Geef een zin die begint met een hoofdletter en eindigt met een punt, vraagteken of uitroepteken: ");
-            zin = toetsenbord.nextLine();
-            begin = zin.charAt(0);
-            einde = zin.charAt(zin.length() - 1);
-            beginLetter = begin >= 'A' && begin <= 'Z';
-            leesteken = einde == '?' ^ einde == '.' ^ einde == '!';
-        } while (beginLetter != leesteken);
-            while (teller < zin.length() - 1){
-                if (zin.charAt(teller) >= 'A' && zin.charAt(teller) <= 'Z'){
-                    hoofdletter = hoofdletter + 1;
-                }else if(zin.charAt(teller) <= '9' && zin.charAt(teller) >= '0'){
-                    cijfers++;
-                }else if(zin.charAt(teller) == ' '){
-                    spaties++;
-                }else {
-                    kleineLetters++;
-                }
-                teller++;
-            }
-            teller = 0;
+        System.out.print("Geef een woord: ");
+        woord = toetsenbord.nextLine();
+        System.out.print("Geef de key: ");
+        key = Integer.parseInt(toetsenbord.nextLine());
 
-        while(teller < zin.length()){
-            if(zin.charAt(teller) <= '9' && zin.charAt(teller) >= '0'){
-                zinCopy = zinCopy + '*';
-            } else {
-                zinCopy = zinCopy + zin.charAt(teller);
-            }
+        while (teller < woord.length()) {
+            karakter = woord.charAt(teller);
+            karakter += key;
+            cypher += karakter;
             teller++;
         }
-
-        System.out.println("Aantal hoofdletter: " + hoofdletter);
-        System.out.println("Aantal kleine letters: " + kleineLetters);
-        System.out.println("Aantal cijfers: " + cijfers);
-        System.out.println("Aantal spaties: "+ spaties);
-        System.out.println(zinCopy);
-
+        System.out.println("Cypher: " + cypher);
     }
 }
